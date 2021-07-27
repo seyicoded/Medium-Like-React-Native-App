96f002a985991fa368125dbed68eaafbbe32bfd1
seyicoded <opadonuseyi01@gmail.com>
2021-07-27 16:57:32 +0100

    wqeq

--- source/screens/user/Bottom_Screen/Settings.js
+++ source/screens/user/Bottom_Screen/Settings.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useEffect, useRef, useContext} from 'react';
+import { View, Text, TextInput, ScrollView, StyleSheet,SafeAreaView,TouchableOpacity, Dimensions, FlatList, ActivityIndicator} from 'react-native'
+import * as CONFIG from '../../../CONFIG'
+import axios from 'axios';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import {Input, Button, Image} from 'react-native-elements'
+import * as Styles from '../Styles'
 
 export default function Settings({navigation, route}) {
+  const context = React.useContext(CONFIG.AppContext);
+    const [Loading, setLoading] = useState(false)
+    const [showError, setshowError] = useState(false)
+    const [showMessage, setshowMessage] = useState(false)
+    const [AlertMessage, setAlertMessage] = useState('Network Error')
+    
+    const error = (msg)=>{
+        setLoading(false)
+        setAlertMessage(msg)
+        setshowError(true)
+    }
+
+    const success = (msg)=>{
+        setLoading(false)
+        setAlertMessage(msg)
+        setshowMessage(true)
+    }
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+        {
+            (Loading) ? <CONFIG.ALERT.Loader /> : <></>
+        }
+        {
+            (showError) ? <CONFIG.ALERT.Alert type="error" message={AlertMessage} hideModal={setshowError} /> : <></>
+        }
+        {
+            (showMessage) ? <CONFIG.ALERT.Alert type="success" message={AlertMessage} hideModal={setshowMessage} /> : <></>
+        }
      </View>
   );
 }
+
+const styles = Styles.Home_Screen;
